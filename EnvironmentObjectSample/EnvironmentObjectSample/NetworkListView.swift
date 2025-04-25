2ea91e2d60ea12ddbeb17d0331433d7a2fadad3f
Mantosh Kumar <kumar.mantosh83@gmail.com>
2025-04-25 22:37:53 +0530

    Network list view added

---
+++ EnvironmentObjectSample/EnvironmentObjectSample/NetworkListView.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkListView.swift
+//  EnvironmentObjectSample
+//
+//  Created by Mantosh Kumar on 25/04/25.
+//
+
+import SwiftUI
+
+struct NetworkListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NetworkListView()
+}
